46dc69cd908f4e601fa2d1e4642f7edcc2c16f52
CatmanFan <30674270+CatmanFan@users.noreply.github.com>
2025-12-27 15:49:35 +0100

    Delete hbm directory

--- hbm/source/utils/i18n/gettext.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HBM_GETMSGTEXT_H_
-#define _HBM_GETMSGTEXT_H_
-
-bool HBM_LoadLanguage(int lang);
-int HBM_GetCurrentLanguage();
-const char* HBM_gettextmsg(const char* msg);
-
-#endif /* _GETTEXT_H_ */
